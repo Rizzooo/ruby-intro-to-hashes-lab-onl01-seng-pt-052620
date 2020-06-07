e372fad98067bfd6a6c59f3626fcf1b78dd840bf
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-07 14:12:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,7 +41,7 @@ def monopoly_with_third_tier
 	        :mortgage_value => "$400"
 	      },
 	      :shortline => {
-	        :mortage_value => "$800"
+	        :mortgage_value => "$800"
 	      }
 	    },
 	    :rent_in_dollars => {
